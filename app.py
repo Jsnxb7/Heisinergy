014bcc5f8709166c5826ef0a7a18761b32f2b337
Anirudhhan <anirudhhan.ashok@gmail.com>
2024-10-25 22:50:18 +0530

    4th commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, redirect, url_for, render_template, make_response
+from flask_cors import CORS
 from werkzeug.security import check_password_hash, generate_password_hash
 import os
 import json
@@ -7,10 +8,15 @@ import base64
 
 
 app = Flask(__name__)
+CORS(app)   
 
 @app.route('/')
 def login():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return "Server is running"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=5000, debug=True)
